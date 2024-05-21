351ad8209dfd7ce504a088d1ce34bac1c3c812c2
Mikhail Beloborodov <kiritohenjanai@gmail.com>
2024-05-21 23:45:03 +0400

    fix bug with moving frame

--- RangeDetector.lua
+++ RangeDetector.lua
@@ -1,3 +1,5 @@
+hasAddonBeenLoaded = false
+
 function Print(msg, r, g, b)
 	DEFAULT_CHAT_FRAME:AddMessage(msg, r, g, b)
 end
@@ -8,6 +10,20 @@ end
 
 function PrintWhite(msg)
 	Print(msg, 1.0, 1.0, 1.0)
+end
+
+function checkRange()
+	if UnitIsFriend("target", "player") then return end
+	if UnitHealth("target") == 0 then return end
+	if IsActionInRange(RangeDetector.slot) == 1 then
+		RangeDetector_bg:SetTexture(0, 1, 0, 0.5);
+	else
+		RangeDetector_bg:SetTexture(1, 0, 0, 0.5);
+	end
+end
+
+function lostTarget()
+	RangeDetector_bg:SetTexture(1, 0, 0, 0.2);
 end
 
 SLASH_RDT1 = "/rdt"
@@ -27,13 +43,13 @@ end
 eventEmitter = CreateFrame("Frame", "EventEmitter")
 eventEmitter.Events = {TARGET_CHANGED = {}, TARGET_LOST ={}}
 eventEmitter.HasTarget = false
-function eventEmitter:AddSubscriber(event, sub, callback, arg)
-	self.Events[event][sub] = {callback = callback, arg = arg}
+function eventEmitter:AddSubscriber(event, sub, callback)
+	self.Events[event][sub] = {callback = callback}
 end
 function eventEmitter:EmitEvent(event)
 	if eventEmitter.HasTarget then
 		for _, subscriber in pairs(self.Events[event]) do
-			subscriber.callback(subscriber.arg)
+			subscriber.callback()
 		end
 	end
 end
