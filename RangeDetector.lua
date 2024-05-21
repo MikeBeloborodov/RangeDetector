bac098a7c76b9d952510d51ff6467c00c5e82ecb
Mikhail Beloborodov <kiritohenjanai@gmail.com>
2024-05-22 00:49:31 +0400

    removed comments \ util funcs

--- RangeDetector.lua
+++ RangeDetector.lua
@@ -1,17 +1,5 @@
 hasAddonBeenLoaded = false
 
-function Print(msg, r, g, b)
-	DEFAULT_CHAT_FRAME:AddMessage(msg, r, g, b)
-end
-
-function PrintRed(msg)
-	Print(msg, 1.0, 0.2, 0.2)
-end
-
-function PrintWhite(msg)
-	Print(msg, 1.0, 1.0, 1.0)
-end
-
 function checkRange()
 	if UnitIsFriend("target", "player") then return end
 	if UnitHealth("target") == 0 then return end
@@ -28,10 +16,6 @@ end
 
 SLASH_RDT1 = "/rdt"
 SlashCmdList["RDT"] = function(msg)
-	--local name = RangeDetector:GetName()
-	--local height = RangeDetector:GetHeight()
-	--local width = RangeDetector:GetWidth()
-	--local point, relativeTo, relativePoint, xOfs, yOfs = RangeDetector:GetPoint()
 	MenuFrame:Show()
 end
 
@@ -43,6 +27,7 @@ end
 eventEmitter = CreateFrame("Frame", "EventEmitter")
 eventEmitter.Events = {TARGET_CHANGED = {}, TARGET_LOST ={}}
 eventEmitter.HasTarget = false
+
 function eventEmitter:AddSubscriber(event, sub, callback)
 	self.Events[event][sub] = {callback = callback}
 end
@@ -53,6 +38,7 @@ function eventEmitter:EmitEvent(event)
 		end
 	end
 end
+
 eventEmitter:SetScript("OnUpdate", 
 	function()
 		if UnitExists("playertarget") then
